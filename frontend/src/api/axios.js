bc78f1d148a467a255effaf643b0e5e63d841852
Sutari Chaitra <83217099+chaitrasutari@users.noreply.github.com>
2024-12-09 10:56:47 -0500

    added api integration

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // Replace with your backend's base URL
+  baseURL: "https://journalz.onrender.com/api", // Replace with your backend's base URL
 });
 
 // Attach Authorization token from cookies to every request
